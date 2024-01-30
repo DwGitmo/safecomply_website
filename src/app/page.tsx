7d50d15961d2372b0fa76ae2ef77236661f1fa37
DwGitmo <david.nycsoftware@gmail.com>
2024-01-30 15:32:27 +0000

    homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,10 @@ import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
+                     
     <main className="flex min-h-screen flex-col items-center justify-between p-24" >
-      <div className="z-10 max-w-5xl w-full fixed float-right items-center justify-end font-mono text-sm lg:flex" >
+                      
+      <div className="z-10 max-w-5xl w-full fixed float-right justify-end font-mono text-sm lg:flex" >
         <div className="fixed float-right flex h-48 w-full items-end justify-end bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <Link         
             className="pointer-events-none float-right flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
