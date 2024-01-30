4b8b3862e22411351878faf9c4e28101e8faad30
DwGitmo <david.nycsoftware@gmail.com>
2024-01-30 15:19:03 +0000

    fotter fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer>
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
         <Link
           href="/about"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -44,7 +44,7 @@ export default function Footer() {
           </p>
         </Link>
 
-        <Link href="/contact"
+       {/*} <Link href="/contact"
           className="group rounded-lg border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
@@ -56,7 +56,7 @@ export default function Footer() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
             Information on how to work with us, contact us today for a free consultation
           </p>
-        </Link>
+  </Link>*/}
       </div>
     </footer>
   )
