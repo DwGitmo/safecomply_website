473ebae08d02666a10befcc63f9d7bf1c1f8f109
DwGitmo <david.nycsoftware@gmail.com>
2024-01-30 15:23:14 +0000

    border delete

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,10 +4,11 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer>
+                      
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
         <Link
           href="/about"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             About Us{" "}
